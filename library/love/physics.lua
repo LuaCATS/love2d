a32a65f88a9f237165958ca6678be2658ac3a0ce
Farooq Karimi Zadeh <fkz@riseup.net>
2026-04-26 15:34:30 +0330

    Updated meta files

--- library/love/physics.lua
+++ library/love/physics.lua
@@ -491,7 +491,7 @@ function Body:getAngularVelocity() end
 ---
 ---[Open in Browser](https://love2d.org/wiki/Body:getContacts)
 ---
----@return love.Contact[] contacts # A list with all contacts associated with the Body.
+---@return table contacts # A list with all contacts associated with the Body.
 function Body:getContacts() end
 
 ---
@@ -500,7 +500,7 @@ function Body:getContacts() end
 ---
 ---[Open in Browser](https://love2d.org/wiki/Body:getFixtures)
 ---
----@return love.Fixture[] fixtures # A sequence with all fixtures.
+---@return table fixtures # A sequence with all fixtures.
 function Body:getFixtures() end
 
 ---
@@ -529,7 +529,7 @@ function Body:getInertia() end
 ---
 ---[Open in Browser](https://love2d.org/wiki/Body:getJoints)
 ---
----@return love.Joint[] joints # A sequence with the Joints attached to the Body.
+---@return table joints # A sequence with the Joints attached to the Body.
 function Body:getJoints() end
 
 ---
@@ -2989,7 +2989,7 @@ function World:destroy() end
 ---
 ---[Open in Browser](https://love2d.org/wiki/World:getBodies)
 ---
----@return love.Body[] bodies # A sequence with all bodies.
+---@return table bodies # A sequence with all bodies.
 function World:getBodies() end
 
 ---
@@ -3037,7 +3037,7 @@ function World:getContactFilter() end
 ---
 ---[Open in Browser](https://love2d.org/wiki/World:getContacts)
 ---
----@return love.Contact[] contacts # A sequence with all Contacts.
+---@return table contacts # A sequence with all Contacts.
 function World:getContacts() end
 
 ---
@@ -3065,7 +3065,7 @@ function World:getJointCount() end
 ---
 ---[Open in Browser](https://love2d.org/wiki/World:getJoints)
 ---
----@return love.Joint[] joints # A sequence with all joints.
+---@return table joints # A sequence with all joints.
 function World:getJoints() end
 
 ---
